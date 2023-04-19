b08bd74d29d5490db632028c97ca4cec78051c58
Lenovo <Lenovo@DESKTOP-CMCVBV3>
2023-04-19 22:57:35 +0300

    Добавьте файлы проекта.

---
+++ LR19_Sydorenco_202TN/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace LR19_Sydorenco_202TN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.fofOK = new System.Windows.Forms.Button();
+            this.radok = new System.Windows.Forms.TextBox();
+            this.vidrad = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 425);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.fofOK);
+            this.tabPage1.Controls.Add(this.radok);
+            this.tabPage1.Controls.Add(this.vidrad);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 396);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Завдання";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // fofOK
+            // 
+            this.fofOK.Location = new System.Drawing.Point(39, 110);
+            this.fofOK.Name = "fofOK";
+            this.fofOK.Size = new System.Drawing.Size(75, 23);
+            this.fofOK.TabIndex = 4;
+            this.fofOK.Text = "button1";
+            this.fofOK.UseVisualStyleBackColor = true;
+            this.fofOK.Click += new System.EventHandler(this.fofOK_Click);
+            // 
+            // radok
+            // 
+            this.radok.Location = new System.Drawing.Point(220, 22);
+            this.radok.Name = "radok";
+            this.radok.Size = new System.Drawing.Size(431, 22);
+            this.radok.TabIndex = 3;
+            // 
+            // vidrad
+            // 
+            this.vidrad.AutoSize = true;
+            this.vidrad.Location = new System.Drawing.Point(217, 66);
+            this.vidrad.Name = "vidrad";
+            this.vidrad.Size = new System.Drawing.Size(46, 17);
+            this.vidrad.TabIndex = 2;
+            this.vidrad.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Відсортований рядок";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Не відсортований рядок";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(767, 396);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button fofOK;
+        private System.Windows.Forms.TextBox radok;
+        private System.Windows.Forms.Label vidrad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ LR19_Sydorenco_202TN/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR19_Sydorenco_202TN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vidrad.Text = "";
+            fofOK.Text = "OK";
+        }
+
+        private void fofOK_Click(object sender, EventArgs e)
+        {           
+            string rem = "";            
+            rem = string.Join(" ", radok.Text.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries));
+            // За допомогою методу Split ділемо рядок, що знаходиться в radok.Text на масив підрядків і розділяємо їх
+            // розділовим знаком " "
+            // Потім за допомогою методу Join щеплюємо масив підрядків розділовим знаком " "            
+            vidrad.Text = rem;
+        }
+        
+    }
+}
